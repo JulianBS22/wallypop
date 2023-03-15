ff0ac1d72889a9bec01e5f70123d71ad66bcac77
Julian Baez <Julianbsuarez@hotmail.com>
2023-03-15 01:36:58 +0100

    añadiendo estilo ppal

---
+++ anuncio-list/anuncioView.js
@@ -0,0 +1,28 @@
+export function buildAnuncioView(anuncio) {
+    const newAnuncioElement = document.createElement('article');
+    newAnuncioElement.classList.add('anuncio')
+    const anuncioDate = new Date(anuncio.date)
+  
+    newAnuncioElement.innerHTML = `
+      <div class="user-info">
+        <span>${anuncio.handle}</span>
+        <img src="${anuncio.avatar}" />
+      </div>
+      <p>${anuncio.content} - ${anuncioDate.toISOString()}</p>
+      
+    `;
+  
+    return newAnuncioElement;
+  }
+  
+  export function buildSpinnerView() {
+    return `<div class="spinner"><div></div><div></div><div></div></div>`
+  }
+  
+  export function buildErrorLoadingAnuncios() {
+    return `<p class="load-error">Ha habido un problema cargando los anuncios. Inténtalo de nuevo más tarde</p>`
+  }
+  
+  export function buildEmptyAnunciosList() {
+    return `<p>No hay resultados disponibles</p>`;
+  }
