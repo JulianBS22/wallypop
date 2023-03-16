a875801c3e232e2f5fc99e4e7b4e2e4e76bcf545
Julian Baez <Julianbsuarez@hotmail.com>
2023-03-16 01:39:24 +0100

    pelea con sparrets

--- anuncio-list/anuncios.js
+++ anuncio-list/anuncios.js
@@ -1,8 +1,1 @@
-export async function getAnuncios() {
-    const anunciosUrlOK = 'http://localhost:8000/api/anuncios';
-    
-    const response = await fetch(anunciosUrlOK);
-    const anuncios = await response.json();
-  
-    return anuncios
-  }
+
